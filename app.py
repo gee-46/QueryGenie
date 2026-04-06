173ab434c9fdef5cdf91ee018f77e99a5605b842
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-04-07 00:50:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
 
+import streamlit as st
+import os
+import sqlite3
+import pandas as pd
+from google import genai
+
+# ------------------ CONFIG ------------------ #
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# ------------------ GEMINI RESPONSE ------------------ #
